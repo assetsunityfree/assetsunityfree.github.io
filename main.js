ed7d65700d4d6bc172a4db00e4bcc6cdefbb02bb
assets unity free <133932542+assetsunityfree@users.noreply.github.com>
2023-05-18 13:40:24 +0700

    tittle, icon

--- main.js
+++ main.js
@@ -1,9 +1,3 @@
-// let data = [
-//     { Name: "facebook", version: "1.0", link: "https://www.facebook.com/" },
-//     { Name: "google", version: "2.0", link: "https://www.google.com/" },
-//     { Name: "youtube1", version: "1.0", link: "https://www.youtube.com/" },
-// ]
-
 let SHEET_ID = '197XpGtSPCduPMmm0RXqEaphwrjJ6E3RxPSLVKJM5aIs'
 let SHEET_TITLE = 'Unity';
 
@@ -30,38 +24,6 @@ fetch(FULL_URL)
                     link2: sheet.table.rows[i].c[5].v })
     }
     renderTable(data);
-
-    //console.log("tables: " + json.tables);
-    // let player_Name_title = document.getElementById('player_Name_title');
-    // let player_Shoe_title = document.getElementById('player_Shoe_title');
-    // let player_Name = document.getElementById('player_Name');
-    // let player_Shoe = document.getElementById('player_Shoe');
-    // let length = data.table.rows.length;
-
-
-    // player_Name_title.innerHTML = data.table.rows[0].c[0].v;
-    // player_Shoe_title.innerHTML = data.table.rows[0].c[1].v;
-    // let selectOptionPlayer = document.createElement('select');
-    // player_Name.append(selectOptionPlayer);
-
-    // for(let i = 1;i<length;i++){
-
-    //     let NewBoxPlayer = document.createElement('option');
-    //     NewBoxPlayer.id = ("box"+i);
-    //     NewBoxPlayer.className = "Some_Style";
-    //     selectOptionPlayer.append(NewBoxPlayer);
-    //     NewBoxPlayer.innerHTML = data.table.rows[i].c[0].v;
-
-    //     let NewBoxShoe = document.createElement('div');
-    //     let space = document.createElement('hr');
-    //     let space2 = document.createElement('hr');
-    //     NewBoxShoe.id = ("box"+i);
-    //     NewBoxShoe.className = "Some_Style";
-    //     player_Shoe.append(NewBoxShoe);
-    //     player_Shoe.append(space);
-    //     player_Shoe.append(space2);
-    //     NewBoxShoe.innerHTML = data.table.rows[i].c[1].v; 
-
     })
 
 let renderTable = function (arr) {
@@ -73,7 +35,7 @@ let renderTable = function (arr) {
             <th scope="row">${index + 1}</th>
             <td><a href="${item.link1}" target="_blank">${item.name}</a></td>
             <td>${item.version}</td>
-            <td><button class="btn btn-info">Dowload</button></td>
+            <td><a href="${item.link1}" target="_blank"button class="btn btn-info">Dowload</button></td>
         </tr>
         `
     })
